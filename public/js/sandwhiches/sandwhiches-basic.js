bb890225fe955bcc0e6e8375e1fdf94c229be5d2
boardwalkclay1 <boardwalkclay1@gmail.com>
2026-04-23 18:25:34 -0400

    Create sandwhiches-basic.js

---
+++ public/js/sandwhiches/sandwhiches-basic.js
@@ -0,0 +1,56 @@
+export const sandwichesBasic = [
+    {
+        name: "Tuna Sandwich",
+        emoji: "🥪",
+        image: "https://source.unsplash.com/featured/?tuna,sandwich",
+        ingredients: ["canned tuna", "mayo", "bread", "lettuce"],
+        directions: [
+            "Mix tuna + mayo.",
+            "Spread on bread.",
+            "Add lettuce and seasonings."
+        ],
+        variations: [
+            "Add pickles.",
+            "Add boiled egg.",
+            "Toast the bread."
+        ],
+        mealPrep: [
+            "Tuna mix lasts 3 days.",
+            "Assemble fresh for best texture."
+        ],
+        links: {
+            walmart: "https://www.walmart.com/search?q=tuna+sandwich+ingredients",
+            kroger: "https://www.kroger.com/search?query=tuna",
+            meijer: "https://www.meijer.com/shopping/search.html?q=tuna",
+            target: "https://www.target.com/s?searchTerm=tuna",
+            google: "https://www.google.com/search?q=tuna+sandwich+ingredients"
+        }
+    },
+
+    {
+        name: "PB&J",
+        emoji: "🥜",
+        image: "https://source.unsplash.com/featured/?peanutbutter,jelly,sandwich",
+        ingredients: ["peanut butter", "jelly", "bread"],
+        directions: [
+            "Spread peanut butter.",
+            "Spread jelly.",
+            "Press together."
+        ],
+        variations: [
+            "Use honey instead of jelly.",
+            "Add banana slices."
+        ],
+        mealPrep: [
+            "Lasts 1 day assembled.",
+            "Great cheap snack."
+        ],
+        links: {
+            walmart: "https://www.walmart.com/search?q=peanut+butter+jelly",
+            kroger: "https://www.kroger.com/search?query=peanut%20butter",
+            meijer: "https://www.meijer.com/shopping/search.html?q=peanut+butter",
+            target: "https://www.target.com/s?searchTerm=peanut+butter",
+            google: "https://www.google.com/search?q=pb+j+ingredients"
+        }
+    }
+];
