4de04903edf4bb5f1edc3369c087a55931f6d6e5
boardwalkclay1 <boardwalkclay1@gmail.com>
2026-04-23 18:29:28 -0400

    Create drinks-cold.js

---
+++ public/js/drinks-cold.js
@@ -0,0 +1,74 @@
+export const drinksCold = [
+    {
+        name: "Sweet Tea",
+        emoji: "🧊",
+        image: "https://source.unsplash.com/featured/?sweet,tea",
+        directions: [
+            "Boil water.",
+            "Steep tea bags 10 minutes.",
+            "Add sugar while hot.",
+            "Chill 2 hours."
+        ],
+        mealPrep: [
+            "Lasts 5 days in fridge.",
+            "Serve over ice.",
+            "Add lemon for flavor."
+        ],
+        pairsWith: ["sandwiches", "rice bowls", "wraps"],
+        links: {
+            walmart: "https://www.walmart.com/search?q=tea+bags",
+            kroger: "https://www.kroger.com/search?query=tea%20bags",
+            meijer: "https://www.meijer.com/shopping/search.html?q=tea+bags",
+            target: "https://www.target.com/s?searchTerm=tea+bags",
+            google: "https://www.google.com/search?q=tea+bags+price"
+        }
+    },
+
+    {
+        name: "Lemonade",
+        emoji: "🍋",
+        image: "https://source.unsplash.com/featured/?lemonade",
+        directions: [
+            "Mix lemon juice + sugar + water.",
+            "Stir until dissolved.",
+            "Serve cold."
+        ],
+        mealPrep: [
+            "Lasts 4 days.",
+            "Add mint for freshness.",
+            "Use sparkling water for fizz."
+        ],
+        pairsWith: ["sandwiches", "fried foods"],
+        links: {
+            walmart: "https://www.walmart.com/search?q=lemons",
+            kroger: "https://www.kroger.com/search?query=lemons",
+            meijer: "https://www.meijer.com/shopping/search.html?q=lemons",
+            target: "https://www.target.com/s?searchTerm=lemons",
+            google: "https://www.google.com/search?q=lemonade+ingredients"
+        }
+    },
+
+    {
+        name: "Kool-Aid",
+        emoji: "🟥",
+        image: "https://source.unsplash.com/featured/?koolaid",
+        directions: [
+            "Mix packet + sugar + water.",
+            "Stir well.",
+            "Chill before serving."
+        ],
+        mealPrep: [
+            "Lasts 5 days.",
+            "Cheap and fast.",
+            "Kids love it."
+        ],
+        pairsWith: ["anything cheap and fast"],
+        links: {
+            walmart: "https://www.walmart.com/search?q=kool+aid",
+            kroger: "https://www.kroger.com/search?query=kool%20aid",
+            meijer: "https://www.meijer.com/shopping/search.html?q=kool+aid",
+            target: "https://www.target.com/s?searchTerm=kool+aid",
+            google: "https://www.google.com/search?q=kool+aid+price"
+        }
+    }
+];
