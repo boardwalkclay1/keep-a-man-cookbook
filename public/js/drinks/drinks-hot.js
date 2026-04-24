d8209e6384515d2c6d1ac9502f50a17d48045c2e
boardwalkclay1 <boardwalkclay1@gmail.com>
2026-04-23 20:11:41 -0400

    Create drinks-hot.js

---
+++ public/js/drinks/drinks-hot.js
@@ -0,0 +1,49 @@
+export const drinksHot = [
+    {
+        name: "Hot Coffee",
+        emoji: "☕",
+        image: "https://source.unsplash.com/featured/?coffee",
+        directions: [
+            "Brew coffee.",
+            "Add sugar or cream.",
+            "Serve hot."
+        ],
+        mealPrep: [
+            "Make a batch and reheat.",
+            "Store in thermos.",
+            "Add flavored syrups."
+        ],
+        pairsWith: ["breakfast wraps", "egg sandwiches"],
+        links: {
+            walmart: "https://www.walmart.com/search?q=coffee",
+            kroger: "https://www.kroger.com/search?query=coffee",
+            meijer: "https://www.meijer.com/shopping/search.html?q=coffee",
+            target: "https://www.target.com/s?searchTerm=coffee",
+            google: "https://www.google.com/search?q=coffee+price"
+        }
+    },
+
+    {
+        name: "Hot Chocolate",
+        emoji: "🍫",
+        image: "https://source.unsplash.com/featured/?hot,chocolate",
+        directions: [
+            "Heat milk or water.",
+            "Mix cocoa powder or packet.",
+            "Stir until smooth."
+        ],
+        mealPrep: [
+            "Lasts 2 days refrigerated.",
+            "Reheat gently.",
+            "Add marshmallows."
+        ],
+        pairsWith: ["desserts", "sweet breakfasts"],
+        links: {
+            walmart: "https://www.walmart.com/search?q=hot+chocolate",
+            kroger: "https://www.kroger.com/search?query=hot%20chocolate",
+            meijer: "https://www.meijer.com/shopping/search.html?q=hot+chocolate",
+            target: "https://www.target.com/s?searchTerm=hot+chocolate",
+            google: "https://www.google.com/search?q=hot+chocolate+price"
+        }
+    }
+];
