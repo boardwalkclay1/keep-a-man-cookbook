12cd9fd0266e708621b957bba3ec684553ccb613
boardwalkclay1 <boardwalkclay1@gmail.com>
2026-04-23 20:26:25 -0400

    Create drinks-master.js

---
+++ public/js/drinks/drinks-master.js
@@ -0,0 +1,9 @@
+import { drinksCold } from "./drinks-cold.js";
+import { drinksHot } from "./drinks-hot.js";
+import { drinksSmoothies } from "./drinks-smoothies.js";
+
+export const DRINK_ITEMS = [
+    ...drinksCold,
+    ...drinksHot,
+    ...drinksSmoothies
+];
