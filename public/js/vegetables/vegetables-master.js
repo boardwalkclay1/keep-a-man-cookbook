7f167a11db677f2e584b446844067452141c133e
boardwalkclay1 <boardwalkclay1@gmail.com>
2026-04-23 20:24:50 -0400

    Create vegetables-master.js

---
+++ public/js/vegetables/vegetables-master.js
@@ -0,0 +1,16 @@
+import vegetablesFresh from "./vegetables-fresh.js";
+import vegetablesFrozen from "./vegetables-frozen.js";
+import vegetablesCanned from "./vegetables-canned.js";
+
+import vegetablesFreshV2 from "./vegetables-fresh-v2.js";
+import vegetablesFrozenV2 from "./vegetables-frozen-v2.js";
+import vegetablesCannedV2 from "./vegetables-canned-v2.js";
+
+export const VEGETABLE_ITEMS = [
+    ...vegetablesFresh,
+    ...vegetablesFrozen,
+    ...vegetablesCanned,
+    ...vegetablesFreshV2,
+    ...vegetablesFrozenV2,
+    ...vegetablesCannedV2
+];
