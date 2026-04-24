16f2d2ce306c846f9d814a0a64940d4e5b7169dc
boardwalkclay1 <boardwalkclay1@gmail.com>
2026-04-23 20:24:18 -0400

    Create filler-master.js

---
+++ public/js/filler/filler-master.js
@@ -0,0 +1,9 @@
+import fillerRice from "./filler-rice.js";
+import fillerNoodles from "./filler-noodles.js";
+import fillerBread from "./filler-bread.js";
+
+export const FILLER_ITEMS = [
+    ...fillerRice,
+    ...fillerNoodles,
+    ...fillerBread
+];
